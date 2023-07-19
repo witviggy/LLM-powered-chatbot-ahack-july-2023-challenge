72694a1893b87eedb67724b1b0b370521fb237e0
Vignesh M <85077492+witviggy@users.noreply.github.com>
2023-07-19 21:54:32 +0530

    Update heatmap.py

--- pages/heatmap.py
+++ pages/heatmap.py
@@ -1,21 +1,40 @@
-def heatmap(HP, Attack, Defense, Sp. Atk, Sp. Def, Speed):
-# Assuming you have a DataFrame named 'df' containing Pokémon data
+import plotly.graph_objects as go
+from plotly.offline import iplot
 
-# Prepare the Pokémon data in a 2D array format
-pokemon_data = df[['HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed']].values
-
-# Create and show the figure
-fig = go.Figure()
-
-fig.add_trace(go.Heatmap(
-    z=pokemon_data,
-    colorbar=dict(
-        title="Stats",
-        titleside="top",
-        tickmode="array",
-        tickvals=[0, 50, 100, 150, 200],
-        ticks="outside"
+def heatmap(df):
+    # Select the top 10 ranked Pokemon based on the "Rank" column
+    top_10 = df.nsmallest(10, "Rank")
+    
+    # Prepare the Pokemon data for the heatmap in a 2D array format
+    pokemon_data = top_10[['HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed']].values
+    
+    # Create the figure
+    fig = go.Figure()
+    
+    # Add the heatmap trace
+    fig.add_trace(go.Heatmap(
+        z=pokemon_data,
+        text=top_10['Names'],  # Set the text to display Pokemon names from the dataset column
+        hovertemplate='Pokemon: %{text}<extra></extra>',  # Customize the hover template--------------------------------------> name isnt displaying.check it.
+        colorbar=dict(
+            title="Stats",
+            titleside="top",
+            tickmode="array",
+            tickvals=[0, 50, 100, 150, 200],
+            ticks="outside"
+        )
+    ))
+    
+    # Set the layout and display the figure
+    layout = go.Layout(
+        title="Heatmap of Top 10 Ranked Pokemon",
+        xaxis=dict(title="Attributes"),
+        yaxis=dict(title="Rank")
     )
-))
+    
+    fig.update_layout(layout)
+    
+    return iplot(fig, filename='Top10PokemonHeatmap')
 
-return fig.show()
+# Call the function with the DataFrame containing the Pokemon data
+heatmap(df)
